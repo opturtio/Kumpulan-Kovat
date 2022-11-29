5994563d5be79cfc33af82ed5899860f097d3ceb
Vili Sirkeoja <vili.sirkeoja@helsinki.fi>
2022-11-29 21:40:08 +0200

    Add route /new_citation

--- routes.py
+++ routes.py
@@ -12,3 +12,20 @@ def citations():
     result = db.session.execute("SELECT citation_name, title, published, author FROM citations")
     citations = result.fetchall()
     return render_template("citations.html", count=len(citations), citations=citations)
+
+@app.route("/new_citation", methods=["GET", "POST"])
+def new_citation():
+    if request.method == "GET":
+        return render_template("new_citation.html")
+    
+    if request.method == "POST":
+        citation_name = request.form["citation_name"]
+        title = request.form["title"]
+        published = request.form["published"]
+        author = request.form["author"]
+
+        sql = """INSERT INTO citations (citation_name, title, published, author)
+                 VALUES (:citation_name, :title, :published, :author)"""
+        db.session.execute(sql, {"citation_name":citation_name, "title":title, "published":published, "author":author})
+        db.session.commit()
+        return redirect("/new_citation")
