3a63e0186a5105e4130557016fe3c45637db37cc
akskokki <akseli.kokki@gmail.com>
2022-12-05 19:32:11 +0200

    Add unit test for bibtex_formatter

--- bibtex_generator/entities/bibtex_formatter.py
+++ bibtex_generator/entities/bibtex_formatter.py
@@ -1,8 +1,13 @@
 import html
 from pylatexenc.latexencode import unicode_to_latex
+from entities.citation import Citation
 
 def create_bibtex_citation(citation):
-    bibtex_string = f"@{citation.type}{{{citation.citation_name}\n"
+    if type(citation) is Citation:
+        citation = citation.get_data()
+    type_key = "type"
+    name_key = "citation_name"
+    bibtex_string = f"@{citation[type_key]}{{{citation[name_key]}\n"
     for key in citation.keys():
         if key != "id" and key != "citation_name" and key != "type" and citation[key]:
             bibtex_string += f"\t{key}: \"{unicode_to_latex(citation[key])}\"\n"
